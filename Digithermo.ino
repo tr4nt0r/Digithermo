24bf58a498957aaa2af2ad229a211d6ea2a6f643
Mr. Bubbles <manni@zapto.de>
2019-12-20 00:14:44 +0100

    major changes, first working version

--- Digithermo.ino
+++ Digithermo.ino
@@ -5,27 +5,16 @@
 #include <DallasTemperature.h>
 #include <OneWire.h>
 
-const uint8_t numbers[10] = {
-	0b11111100,  //0
-	0b01100000,  //1
-	0b11011010,  //2
-	0b11110010,  //3
-	0b01100110,  //4
-	0b10110110,  //5
-	0b10111110,  //6
-	0b11100000,  //7
-	0b11111110,  //8
-	0b11110110,  //9
-};
-
-const uint8_t letter_E = 0b10011110;
-const uint8_t letter_r = 0b00001010;
-const uint8_t symbol_minus = 0b10000000;
+const uint8_t numbers[10] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6f };
+const uint8_t letter_E = 0x79;
+const uint8_t letter_r = 0x50;
+const uint8_t symbol_minus = 0x40;
 
 const uint8_t digit2pin[4] = { 16, 19, 18, 17 };
 const uint8_t SENSOR_PIN = 2;
 uint8_t active = 0;
-volatile uint16_t temperature;
+uint8_t digit = 0;
+volatile int16_t temperature;
 volatile bool sensorError = false;
 
 OneWire wire(SENSOR_PIN);
@@ -33,14 +22,13 @@ DallasTemperature sensors(&wire);
 
 void setup()
 {
-	DDRB = 0xFE; //set PB0 - PB6 as Output
-	DDRC = 0x3C; //set PC2 - PC5 as Output
+	DDRB = 0xFF; //set PB0 - PB6 as Output
+	DDRC = 0xFF; //set PC2 - PC5 as Output
 
 	Timer1.initialize(5000);
-	Timer1.attachInterrupt(displayMultiplex);
+	Timer1.attachInterrupt(displayMultiplex);	
 	selftest();
 	sensors.begin();
-
 }
 
 void loop()
@@ -48,49 +36,52 @@ void loop()
 	//wait for sensor to connect
 	while (!sensors.requestTemperaturesByIndex(0)) {
 		sensorError = true;
-		delay(100);
+		delay(1000);
 	}
 	sensorError = false;
 	noInterrupts();
-	temperature = sensors.getTempCByIndex(0)*10;
+	temperature = (int16_t)(sensors.getTempCByIndex(0) * 10.0);
 	interrupts();
 	delay(1000);
 }
 
 void selftest() {
 	//7-segment display selftest
-	uint8_t i = 9;
-	while (i --> 0) {
+	uint8_t i = 10;
+	while (i-- > 0) {
 		temperature = 1111 * i;
-		delay(500);
+		delay(50000);
 	}
 }
 
 
 void displayMultiplex() {
 	digitalWrite(digit2pin[active], LOW);  //disable last digit
-	active = (active ==  3) ? 0 : active + 1; //go to next digit
+	active = (active == 3) ? 0 : active + 1; //go to next digit
 	digitalWrite(digit2pin[active], HIGH); //enable next digit
-
+	PORTB = 0;
 	if (!sensorError) {
-		if(temperature < 0 && active == 3) {
+
+		if (temperature < 0 && active == 3) {
 			PORTB = symbol_minus;
-		} else {
-			uint8_t digit = abs(temperature) / pow(10, active + 1) % 10;  //get number to display on active digit
-			PORTB = numbers[digit];  //Set I/Os for digit#
 		}
-	}
-	else {
+		else if (abs(temperature) < pow(10, active) && active > 1){  //don't display leading zero
+			PORTB = 0;
+		} else {
+			digit = abs(temperature) / pow(10, active) % 10;  //get number to display on active digit
+			PORTB = numbers[digit];  //Set I/Os for digit
+		}
+	} else {
 		//display Err 
 		switch (active) {
-		case 3:
-			PORTB = 0; //Digit off			
+		case 0:
+			PORTB = 0x00; //Digit off			
 			break;
-		case 2:
+		case 3:
 			PORTB = letter_E;
 			break;
+		case 2:
 		case 1:
-		case 0:
 			PORTB = letter_r;
 			break;
 		}
